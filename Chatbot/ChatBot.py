81f5566a364d71fd5c11cd1d2543860e0437253a
gkonen <guillaumekonen@gmail.com>
2025-06-27 16:47:30 +0200

    - Add Third project

---
+++ Chatbot/ChatBot.py
@@ -0,0 +1,142 @@
+#region STAGE 1 : Meet your chatbot
+
+print("Hello! My name is Aid")
+print("I was created in 2025")
+
+#endregion
+
+#region STAGE 2 : Introduce yourself
+
+def greet(bot_name, birth_year):
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {birth_year}.")
+
+
+def remind_name():
+    print("Please, remind me your name.")
+    your_name = input()
+    print(f"What a great name you have, {your_name}!")
+
+
+greet("Aid", 2023)
+remind_name()
+
+#endregion
+
+#region STAGE 3 : Let the chatbot guess your age
+
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
+
+#endregion
+
+#region STAGE 4 : Teach your bot to count
+
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
+
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+
+# read a number and count to it here
+count = int(input())
+for i in range(count + 1):
+    print(f"{i} !")
+
+print('Completed, have a nice day!')
+
+#endregion
+
+#region STAGE 5 : Play a simple quiz together
+
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+
+    print("Why  do we use methods?")
+    print("1. To repeat a statement multiple times.")
+    print("2. To decompose a program into several small subroutines.")
+    print("3. To determine the execution time of a program.")
+    print("4. To interrupt the execution of a program.")
+    while True:
+        choice = int(input("> "))
+        if choice == 2:
+            break
+        else:
+            print("Please, try again.")
+
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
+
+#endregion
